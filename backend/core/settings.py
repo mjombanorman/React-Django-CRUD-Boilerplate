6a80559867fa3cacef05a477b6149555bf224a82
mjombanorman <mjomban@gmail.com>
2024-01-08 17:01:39 +0300

    Complete setup of the frontend application with basic components and routers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,9 +29,11 @@ INSTALLED_APPS = [
     # installed  applications
     
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -104,3 +106,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Customized Settings
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
