38a0b808d6787d2f2827360d282ddea01cfbdf2e
mjombanorman <mjomban@gmail.com>
2024-01-09 17:15:56 +0300

    Created form elements with Material UI and React Hook Form to allow user perform CRUD operations via Frontend

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,9 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import MyDatePicker from "./forms/MyDatePicker";
+import MySelectField from "./forms/MySelectField";
+import MyTextField from "./forms/MyTextField";
+import MyTextAreaField from "./forms/MyTextAreaField";
+import { useForm } from "react-hook-form";
 const Create = () => {
-  return (
-    <div>Create</div>
-  )
-}
+  const { handleSubmit, reset, setValue, control } = useForm();
 
-export default Create
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          backgroundColor: "lightblue",
+          marginBottom: "10px",
+        }}>
+        <Typography sx={{ marginLeft: "20px", color: "#d01500" }}>
+          Create Records
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          border: "2px solid gray",
+          paddng: "20px",
+          flexDirection: "column",
+        }}>
+        <Box
+          sx={{
+            padding: "20px",
+          }}>
+          <form onSubmit={handleSubmit((data) => console.log(data))}>
+            <MyTextField
+              label="Name"
+              placeholder="Name"
+              control={control}
+              name="name"
+            />
+            <MyDatePicker
+              label="Start Date"
+              control={control}
+              name="start_date"
+            />
+            <MyDatePicker label="End Date" control={control} name="end_date" />
+            <MySelectField label="Status" control={control} name="status" />
+            <MyTextAreaField label="Comment" control={control} name="comment" />
+            <Button variant="outlined" type="submit" sx={{marginTop:"20px"}}
+                    >
+   
+              Save
+            </Button>
+         
+          </form>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Create;
---
+++ frontend/src/components/forms/MySelectField.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Controller } from "react-hook-form";
+
+export default function MySelectField(props) {
+
+  const { label, placeholder, name, control } = props;
+
+
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="select-label">{label}</InputLabel>
+        <Controller
+          name={name}
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <Select
+              onChange={onChange}
+              value={value}
+              label={label}
+              name={name}>
+              <MenuItem value={"open"}>Open</MenuItem>
+              <MenuItem value={"in-progress"}>In Progress</MenuItem>
+              <MenuItem value={"closed"}>Closed</MenuItem>
+            </Select>
+          )}
+        />
+      </FormControl>
+    </Box>
+  );
+}
