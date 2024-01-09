c32dfd48ccd0de2020ef5607306097a5ec8c7922
mjombanorman <mjomban@gmail.com>
2024-01-09 10:55:37 +0300

    Responsive Navigation Menu with Material UI Components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
+import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <NavBar
+        drawerWidth={220}
+        content={
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        }
+      />
     </div>
   );
 }
